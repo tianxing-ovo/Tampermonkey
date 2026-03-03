642fc511d279149f9c863440c825ea9d00cc8808
20503 <2050365570@qq.com>
2026-03-04 02:21:52 +0800

    chore(translation): 更新翻译词条并调整资源版本号
    - 新增多条仓库相关功能的中文翻译，如归档仓库、构建和部署、修改仓库可见性等
    - 添加危险区域、删除仓库、自定义域名等多项新翻译词条
    - 更新汉化脚本中翻译资源文件版本号至v3
    - 丰富翻译内容以支持更多GitHub界面操作提示和功能说明

--- 汉化脚本.js
+++ 汉化脚本.js
@@ -12,7 +12,7 @@
 // @match        https://stackoverflow.com/*
 // @match        https://huggingface.co/*
 // @match        https://github.com/*
-// @resource     translations https://raw.githubusercontent.com/tianxing-ovo/Tampermonkey/master/translations.json?v=2
+// @resource     translations https://raw.githubusercontent.com/tianxing-ovo/Tampermonkey/master/translations.json?v=3
 // @grant        GM_getResourceText
 // @run-at       document-start
 // @license      Apache-2.0
